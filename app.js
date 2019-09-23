515b7817627d612affff7294ba892e866bd47f2c
Chokri K <khalifa.chokri@gmail.com>
2019-09-23 17:47:12 +0200

    Add a argv limit

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
-var fs = require('fs'),
-    request = require('request');
+const fs = require("fs"),
+  request = require("request");
+const download = function(uri, filename, callback) {
+  request.head(uri, function(err, res, body) {
+    console.log("content-type:", res.headers["content-type"]);
+    console.log("content-length:", res.headers["content-length"]);
 
-var download = function(uri, filename, callback){
-  request.head(uri, function(err, res, body){
-    console.log('content-type:', res.headers['content-type']);
-    console.log('content-length:', res.headers['content-length']);
-
-    request(uri).pipe(fs.createWriteStream(`img/${filename}`)).on('close', callback);
+    request(uri)
+      .pipe(fs.createWriteStream(`img/${filename}`))
+      .on("close", callback);
   });
 };
-for(let i = 0; i < 1000;i++){
-  download(`https://picsum.photos/id/${i}/1920/1080`, `image-${i}.png`, function(){
-    console.log(`${i} image downloaded.`);
-  });
+
+const limit =
+  process.argv.slice(2)[0] == undefined ? 10 : process.argv.slice(2)[0];
+
+for (let i = 0; i < limit; i++) {
+  download(
+    `https://picsum.photos/id/${i}/1920/1080`,
+    `image-${i}.png`,
+    function() {
+      console.log(`${i} image downloaded.`);
+    }
+  );
 }
